e5c1f242ae5307bdab0c7da5aa31371ea0afe20c
Chip Lukes <34627158+chiplukes@users.noreply.github.com>
2024-05-04 17:23:04 -0600

    add ignore for ruff S101

--- tests/test_example_python_package.py
+++ tests/test_example_python_package.py
@@ -1,4 +1,5 @@
 from python_example_package import python_example_package_function
 
+
 def test_main_module_function():
-    assert python_example_package_function() == True
+    assert python_example_package_function() is True
